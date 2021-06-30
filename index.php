14fd8d16dd6758e8e69de00433a52c9751b958c8
patriziosbr <sbrozzi.patrizio@gmail.com>
2021-06-30 17:28:34 +0200

    style da rivedere

--- index.php
+++ index.php
@@ -6,26 +6,40 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="css/style.css">
         <title>Document</title>
     </head>
     <body>
         
         <header>
-            <h1>Title</h1>
+            <?php include 'partials/template/header.php' ?>
         </header>
 
         <main>
-            
+            <div class="container">
+                <div class="card-cont">
+                
+                    <?php foreach ($albums as $album) { ?>
+                        <div class="card">
+                            <img class="cover" src='<?= $album["poster"];?>' alt="albums error"> 
+                            <div class="infoAlbum">
+                                <h2>Title: <strong><?= $album["title"]?> </strong></h2>
+                                <h3>Author: <strong><?= $album["author"]?> </strong></h3>
+                                <h4>Genere: <strong><?= $album["genre"]?> </strong></h4>
+                                <h5>Year: <strong><?= $album["year"]?></strong></h5>
 
-            <?php foreach ($albums as $album) { ?>
-
-
-               <img src='<?= $album["poster"];?>' alt="albums error"> 
+                            </div>
+                        </div>
 
 
 
-            <?php } ?>
-            
+                    <?php } ?>    
+
+                </div>
+            </div>
 
         </main>
 
---
+++ partials/template/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/headerStyle.css">
+</head>
+<body>
+    <div class="container">
+        <div class="home">
+            <a href="#">
+                <img src="https://i.ibb.co/n6SbsZY/1-Risorsa-1.png" alt="logo">
+                <h1>Puccio play</h1>
+            </a>
+        </div>
+    </div>
+</body>
+</html>
